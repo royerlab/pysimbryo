deeb86a95e411137f76200e6d953b68ffc122ffa
Loic Alain Royer <royerloic@gmail.com>
2018-07-14 10:09:32 -0700

    fixed gradle wrapper

--- src/java/AdditionApplication.java
+++ src/java/AdditionApplication.java
@@ -1,16 +1,20 @@
 import py4j.GatewayServer;
 
-public class AdditionApplication {
 
-  public int addition(int first, int second) {
+
+public class AdditionApplication
+{
+
+  public int addition(int first, int second)
+  {
     return first + second;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
     AdditionApplication app = new AdditionApplication();
     // app is now the gateway.entry_point
     GatewayServer server = new GatewayServer(app);
     server.start();
   }
 }
-
